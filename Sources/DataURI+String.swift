3d2cf10bec4eef4352eb15e35040219434d191f8
Valentin Corral <valencg90@gmail.com>
2017-04-25 16:58:03 +0200

    Updated to Vapor 2

--- Sources/DataURI+String.swift
+++ Sources/DataURI+String.swift
@@ -1,23 +1,6 @@
 import Core
 import Foundation
 
-//FIXME(Brett): I patched this into Core 1.1, remove when updated.
-extension Sequence where Iterator.Element == Byte {
-    internal var base64Decoded: Bytes {
-        let bytes = [Byte](self)
-        let dataBase64 = Data(bytes: bytes)
-        
-        guard let data = Data(base64Encoded: dataBase64) else {
-            return []
-        }
-        
-        var encodedBytes = Bytes(repeating: 0, count: data.count)
-        data.copyBytes(to: &encodedBytes, count: data.count)
-        
-        return encodedBytes
-    }
-}
-
 extension String {
     /**
         Parses a Data URI and returns its data and type.
@@ -26,6 +9,6 @@ extension String {
      */
     public func dataURIDecoded() throws -> (data: Bytes, type: String) {
         let (data, type, _) = try DataURIParser.parse(uri: self)
-        return (data, type.string)
+        return (data, type.makeString())
     }
 }
