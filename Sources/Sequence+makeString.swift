9e9bb2885194940fb7c71525a44fba43cad9e960
Heidi Puk Hermann <heih@nodesagency.com>
2018-12-05 11:35:41 +0100

    Recover Sequence extension - makeString() from Vapor 2

--- Sources/Sequence+makeString.swift
+++ Sources/Sequence+makeString.swift
@@ -1,8 +1,14 @@
-//
-//  Sequence+makeString.swift
-//  DataURI
-//
-//  Created by heidi puk hermann on 05.12.18.
-//
+import Core
 
-import Foundation
+extension Sequence where Iterator.Element == Byte {
+    /// Converts a slice of bytes to
+    /// string. Courtesy of Vapor 2 - Core and @vzsg
+    public func makeString() -> String {
+        let array = Array(self) + [0]
+
+        return array.withUnsafeBytes { rawBuffer in
+            guard let pointer = rawBuffer.baseAddress?.assumingMemoryBound(to: CChar.self) else { return nil }
+            return String(validatingUTF8: pointer)
+            } ?? ""
+    }
+}
