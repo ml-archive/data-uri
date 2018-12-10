739407d948fc6ae7b75fab8bbe2d51be956f17c6
Heidi Puk Hermann <heih@nodesagency.com>
2018-12-10 13:58:42 +0100

    Add new grouped files

---
+++ Sources/DataURI/Helpers/Byte+Alphabet.swift
@@ -0,0 +1,8 @@
+//
+//  Byte+Alphabet.swift
+//  DataURI
+//
+//  Created by heidi puk hermann on 10.12.18.
+//
+
+import Foundation
---
+++ Sources/DataURI/Helpers/Byte+Controllers.swift
@@ -0,0 +1,8 @@
+//
+//  Byte+Controllers.swift
+//  DataURI
+//
+//  Created by heidi puk hermann on 10.12.18.
+//
+
+import Foundation
